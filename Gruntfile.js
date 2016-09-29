8eca0cfb678fd98ff4cfaaa903717a3ef7b858c2
Rinas <rinas@keepworks.com>
2016-09-29 18:20:14 +0530

    use openui5_connect to run a web server

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,52 @@
+'use strict';
+
 module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-openui5');
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     openui5_preload: {
       component: {
         options: {
           resources: {
-            cwd: '',
-            prefix: '',
+            cwd: 'app',
+            prefix: 'sap/ui/app',
             src: [
-              'app/**/*.js',
-              'app/**/*.fragment.xml',
-              'app/**/*.view.xml',
-              'app/**/*.properties'
+              '**/*.js',
+              '**/*.fragment.xml',
+              '**/*.view.xml',
+              '**/*.properties'
             ]
           },
-          dest: '',
+          dest: 'app',
           compress: true
         },
         components: true
       }
+    },
+
+    connect: {
+      server: {
+        options: {
+          keepalive: true,
+          port: 8000
+        }
+      }
+    },
+
+    openui5_connect: {
+      server: {
+        options: {
+          appresources: 'app'
+        }
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-openui5');
-  grunt.registerTask('default', ['openui5_preload']);
+  grunt.registerTask('default', [
+    'openui5_preload',
+    'openui5_connect'
+  ]);
 }
